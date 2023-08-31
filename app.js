a4c50b60a997f72ffc926883a0008cbd0fdcf2ed
George-Ashraf <georgeid20@gmail.com>
2023-08-31 18:53:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+wow = new WOW({
+  boxClass: 'animate__animated'
+
+}).init();
+// wow js
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+});
+// hamburger
+
+
+
+
+
